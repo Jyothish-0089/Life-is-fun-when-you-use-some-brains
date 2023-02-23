f8d1c91b6d1e94f55809892d36c6b9c5a8f72389
Kaizer_Izumi <112233651+Jyothish-0089@users.noreply.github.com>
2023-02-23 08:47:44 +0100

    Create Attendance manager.py

---
+++ Attendance manager.py
@@ -0,0 +1,25 @@
+total = int(input("Total classes: "))
+attended = int(input("Classes attended: "))
+if(total>=attended):
+    goal = 80
+    count = 0
+    percentage = (attended/total)*100
+    print("Percentage: ",round(percentage),"%")
+    if (round(percentage) == goal):
+        print("--->It's exactly ", goal, "%, don't miss your classes ^^")
+    if(round(percentage)<goal):
+        while(round(percentage)<goal):
+            total = total + 1
+            attended = attended + 1
+            percentage = (attended / total) * 100
+            count = count + 1
+        print("--->You need to attend ",count,"classes   (",count//4,"Days and ",count%4,"Classes)")
+    if(round(percentage)>goal):
+        while(round(percentage)>goal):
+            total = total + 1
+            percentage = (attended / total) * 100
+            if(round(percentage)>goal):
+                count = count + 1
+        print("--->You can skip ",count," classes   (",count//4,"Days and ",count%4,"Classes)")
+else:
+    print("---Invalid Input---\nLogic left the chat...")
